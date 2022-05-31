594118b5d1e0bad824e3485fec0dc11ecfc98c57
huangzb <huangzb@si-tech.com.cn>
2022-05-31 17:26:21 +0800

    add 文件下载例子

--- admin/src/main/java/com/sitech/paas/admin/web/controller/MyTestController.java
+++ admin/src/main/java/com/sitech/paas/admin/web/controller/MyTestController.java
@@ -1,13 +1,22 @@
 package com.sitech.paas.admin.web.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * 测试controller
+ *
  * @author huangzb
  * @date 2022/4/24
  */
@@ -17,30 +26,56 @@ import java.util.HashMap;
 public class MyTestController {
 
     @RequestMapping("hello")
-    public String testMethod(){
+    public String testMethod() {
         return "Hello World!";
     }
 
     @RequestMapping("exception")
-    public int getException(){
+    public int getException() {
         HashMap<String, Integer> map = new HashMap<>(8);
-        map.put("1",1);
-        map.put("2",0);
-        return map.get("1")/map.get("2");
+        map.put("1", 1);
+        map.put("2", 0);
+        return map.get("1") / map.get("2");
     }
 
     @RequestMapping("myException")
-    public int myException(){
-        int  i;
+    public int myException() {
+        int i;
         try {
             HashMap<String, Integer> map = new HashMap<>(8);
-            map.put("1",1);
-            map.put("2",0);
-            i = map.get("1")/map.get("2");
-        }catch (Exception e){
+            map.put("1", 1);
+            map.put("2", 0);
+            i = map.get("1") / map.get("2");
+        } catch (Exception e) {
             throw new RuntimeException("除数为0了!");
         }
         return i;
+    }
+
+
+    @RequestMapping("download")
+    public void myDownload(HttpServletResponse response) throws IOException {
+        List<String> row1 = CollUtil.newArrayList("aa", "bb", "cc", "dd");
+        List<String> row2 = CollUtil.newArrayList("aa1", "bb1", "cc1", "dd1");
+        List<String> row3 = CollUtil.newArrayList("aa2", "bb2", "cc2", "dd2");
+        List<String> row4 = CollUtil.newArrayList("aa3", "bb3", "cc3", "dd3");
+        List<String> row5 = CollUtil.newArrayList("aa4", "bb4", "cc4", "dd4");
+        List<List<String>> rows = CollUtil.newArrayList(row1, row2, row3, row4, row5);
+        // 通过工具类创建writer，默认创建xls格式
+        ExcelWriter writer = ExcelUtil.getWriter();
+        // 一次性写出内容，使用默认样式，强制输出标题
+        writer.write(rows, true);
+        //out为OutputStream，需要写出到的目标流
+        //response为HttpServletResponse对象
+        response.setContentType("application/vnd.ms-excel;charset=utf-8");
+        //test.xls是弹出下载对话框的文件名，不能为中文，中文请自行编码
+        response.setHeader("Content-Disposition","attachment;filename=test.xls");
+        ServletOutputStream out=response.getOutputStream();
+        writer.flush(out, true);
+        // 关闭writer，释放内存
+        writer.close();
+        //此处记得关闭输出Servlet流
+        IoUtil.close(out);
     }
 
 }
